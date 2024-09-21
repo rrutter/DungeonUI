358f916e8f915da2bc43648e3dcc12a1769e08ed
Russ Rutter <arius_storm@yahoo.com>
2024-09-21 10:51:17 -0400

    Add some consistency between the dungeon and town guards

--- src/app/dungeon/dungeon.component.ts
+++ src/app/dungeon/dungeon.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router} from '@angular/router';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -7,45 +6,20 @@ import { UserService } from '../services/user.service';
   templateUrl: './dungeon.component.html',
   styleUrl: './dungeon.component.css'
 })
-
 export class DungeonComponent implements OnInit {
-  private selectedCharacter:any;
+  private selectedCharacter: any;
   level!: number;
   xPosition!: number;
   yPosition!: number;
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.selectedCharacter = this.userService.getSelectedCharacter();
-    this.verifyBackendLocation();
     this.loadDungeonRoom(this.selectedCharacter.level, this.selectedCharacter.xPosition, this.selectedCharacter.yPosition);
   }
 
   loadDungeonRoom(level: number, xPosition: number, yPosition: number) {
-
+    // Logic for loading the dungeon room
   }
-
-  verifyBackendLocation(): void {
-    this.userService.getCharacterLocation(this.selectedCharacter.id).subscribe(
-      (locationId: number) => {
-        if (locationId === this.selectedCharacter.locationId) {
-          // Load dungeon room if the location matches
-          this.loadDungeonRoom(this.selectedCharacter.level, this.selectedCharacter.xPosition, this.selectedCharacter.yPosition);
-        } else {
-          // If location doesn't match, redirect to the correct location
-          if (locationId === 0) {
-            this.router.navigate(['/town']);
-          } else {
-            this.router.navigate(['/dungeon']);
-          }
-        }
-      },
-      error => {
-        console.error('Error verifying character location', error);
-        this.router.navigate(['/menu']);
-      }
-    );
-  }
-
 }
--- src/app/guards/dungeon-access.guard.ts
+++ src/app/guards/dungeon-access.guard.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +11,24 @@ export class DungeonAccessGuard implements CanActivate {
 
   constructor(private userService: UserService, private router: Router) {}
 
-  canActivate(): boolean {
+  canActivate(): Observable<boolean> | boolean {
     const selectedCharacter = this.userService.getSelectedCharacter();
-
-    // Check if the character exists and is in a dungeon
-    if (selectedCharacter && selectedCharacter.locationId !== 0) {
-      return true;
-    } else {
-      // If the character is not in a dungeon, redirect to the town
-      this.router.navigate(['/town']);
+    if (!selectedCharacter) {
+      this.router.navigate(['/menu']);
       return false;
     }
+
+    // Check if the character is in a dungeon (locationId != 0)
+    return this.userService.getCharacterLocation(selectedCharacter.id).pipe(
+      map((locationData: any) => {
+        if (locationData.locationId !== 0) {
+          return true;  // Character is in a dungeon
+        } else {
+          // Redirect to the town if the character is not in a dungeon
+          this.router.navigate(['/town']);
+          return false;
+        }
+      })
+    );
   }
 }
